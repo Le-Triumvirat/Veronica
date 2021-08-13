cdb603b377cfc5b585905c0024b5aa825d794b85
PyTorcher1729_36 <v2b2y2k@gmail.com>
2021-08-13 22:42:01 +0530

    Included inspirational quotes

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
 import discord
+import requests
+import json
+
+# import os
+
+def req_info():
+    response = requests.get("https://zenquotes.io/api/random")
+    quote = response.json()[0]['q']
+    author = response.json()[0]['a']
+    final = quote+' - '+author
+    return final
 
 client = discord.Client()
 try:
@@ -12,12 +23,17 @@ try:
     async def on_message(message):
         if(message.author == client.user):
             return
-        if(message.content.startswith('$hello')):
+        elif(message.content == '$hello'):
             await message.channel.send(f'Hello {message.author}')
+        elif(message.content == '$quote'):
+            await message.channel.send(f'{req_info()}')
+        # elif(message.content):
+        #     await message.channel.send(message.content)
 
     f = open('./token.txt','r')
     token = f.read()
     client.run(token)
+    # client.run(os.getenv('TOKEN'))
 
 except KeyboardInterrupt:
     print('Exception handled')

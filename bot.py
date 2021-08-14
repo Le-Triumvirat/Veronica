932a6276ff6a1ce4c110f0b42d21dff271608b48
PyTorcher1729_36 <v2b2y2k@gmail.com>
2021-08-14 13:38:51 +0530

    started with discord.ext

--- bot.py
+++ bot.py
@@ -1,39 +1,30 @@
 import discord
 import requests
 import json
+from discord.ext import commands
 
-# import os
+# commands.Bot class is a sub-class of discord.Client()
+# client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-def req_info():
+# As soon as the bot is ready
+@bot.event
+async def on_ready():
+    print(f'I am {bot.user}')
+
+@bot.command()
+async def test(ctx):
+    await ctx.send(ctx.channel)
+
+@bot.command()
+async def quote(ctx):
     response = requests.get("https://zenquotes.io/api/random")
     quote = response.json()[0]['q']
     author = response.json()[0]['a']
     final = quote+' - '+author
-    return final
+    await ctx.send(final)
 
-client = discord.Client()
-try:
-    '''As soon as the bot is ready'''
-    @client.event
-    async def on_ready():
-        print(f'I am {client.user}')
-
-    '''If it receives a message'''
-    @client.event
-    async def on_message(message):
-        if(message.author == client.user):
-            return
-        elif(message.content == '$hello'):
-            await message.channel.send(f'Hello {message.author}')
-        elif(message.content == '$quote'):
-            await message.channel.send(f'{req_info()}')
-        # elif(message.content):
-        #     await message.channel.send(message.content)
-
-    f = open('./token.txt','r')
-    token = f.read()
-    client.run(token)
-    # client.run(os.getenv('TOKEN'))
-
-except KeyboardInterrupt:
-    print('Exception handled')
+# client.run(os.getenv('TOKEN'))
+f = open('./token.txt','r')
+token = f.read()
+bot.run(token)

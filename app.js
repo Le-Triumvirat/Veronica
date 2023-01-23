5912d329b97e5228801817b16793cab7a4a4d6af
vishnubalaji <v2b2y2k@gmail.com>
2023-01-23 20:48:22 +0530

    new files added

---
+++ app.js
@@ -0,0 +1,14 @@
+// const readline = require("readline")
+// let rl = readline.createInterface(
+//     process.stdin,
+//     process.stdout
+// )
+
+// rl.question("your name?\n", (name) => {
+//     console.log("You're name is "+name)
+//     rl.close()
+// }
+// )
+
+let add = require('./add')
+console.log(add(1,2))
